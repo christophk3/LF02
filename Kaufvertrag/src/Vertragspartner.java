621a7194e7ae08bc71e226342779507de3a733cf
christophk3 <christoph.k3@schule.bremen.de>
2022-03-10 15:27:08 +0100

    Kaufvertrag

---
+++ Kaufvertrag/src/Vertragspartner.java
@@ -0,0 +1,12 @@
+public class Vertragspartner {
+    private String vorname;
+    private String nachname;
+    private String ausweisNr;
+    private Adresse adresse;
+
+    public Vertragspartner(String vorname, String nachname) {
+        this.vorname = vorname;
+        this.nachname = nachname;
+
+    }
+}
