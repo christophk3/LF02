3e72086a34aa56f14999d3de34ebdd4b013831c6
christophk3 <christoph.k3@schule.bremen.de>
2022-03-21 09:01:16 +0100

    ok

--- Kaufvertrag/src/ProgrammReadCSV.java
+++ Kaufvertrag/src/ProgrammReadCSV.java
@@ -3,33 +3,48 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.Objects;
 
 public class ProgrammReadCSV {
     public static void main(String[] args) throws IOException {
         String datei = "H://LF02_//CSV/Kaufvertrag.csv";
         BufferedReader br = new BufferedReader(new FileReader(datei));
         String zeile;
+        Vertragspartner verkaeufer = null;
+        Vertragspartner kaeufer = null;
         while ((zeile = br.readLine()) != null) {
             String str = zeile;
-            String[]array = str.split(";");
+            String[] array = str.split(";");
+
+            String[] name = array[1].split(" ");
+            String[] adresse = array[3].split(" ");
+            String strasse = "";
+            for (int i = 0; i < adresse.length - 1; i++) {
+                strasse += adresse[i] + " ";
+            }
+            strasse = strasse.trim();
+            String[] ort = array[4].split(" ");
+            String ort2 = "";
+            for (int j = 1; j < ort.length; j++) {
+                ort2 = ort[j] + " ";
+            }
+            ort2 = ort2.trim();
             System.out.println(Arrays.toString(array));
-
-            String[]name = array[1].split(" ");
-            String[]adresse = array[2].split(" ");
-
-            if (array[0] == "Verkaeufer") {
-                Vertragspartner verkaeufer = new Vertragspartner(name[0],name[1]);
-                verkaeufer.setAdresse(new Adresse(adresse[0],adresse[1],array[3],array[4]));
-
-
-
+            if (Objects.equals(array[0], "Verkaeufer")) {
+                verkaeufer = new Vertragspartner(name[0], name[1]);
+                verkaeufer.setAusweisNr(array[2]);
+                verkaeufer.setAdresse(new Adresse(strasse.toString(), adresse[adresse.length - 1], ort[0], ort2.toString()));
+            }
+            if (Objects.equals(array[0], "Kaeufer")) {
+                kaeufer = new Vertragspartner(name[0], name[1]);
+                kaeufer.setAusweisNr(array[2]);
+                kaeufer.setAdresse(new Adresse(strasse.toString(), adresse[adresse.length - 1], ort[0], ort2.toString()));
             }
 
 
-
-
-
         }
+        System.out.println(verkaeufer);
+        System.out.println(kaeufer);
 
 
     }
--- Kaufvertrag/src/ProgrammWriteCSV.java
+++ Kaufvertrag/src/ProgrammWriteCSV.java
@@ -3,28 +3,33 @@ import java.io.*;
 public class ProgrammWriteCSV {
     public static void main(String[] args) throws FileNotFoundException {
         Vertragspartner kaeufer = new Vertragspartner("Yannick","Schroeder");
-        kaeufer.setAdresse(new Adresse("Marxstrasse", "31","28279", "Bremen"));
+        kaeufer.setAdresse(new Adresse("Marxstrasse zum Vogel", "31","28279", "Bremen"));
+        kaeufer.setAusweisNr("1234567");
         Vertragspartner verkaeufer = new Vertragspartner("Christoph","Kroeger");
-        verkaeufer.setAdresse(new Adresse("Ledaweg", "64","28359", "Bremen"));
+        verkaeufer.setAdresse(new Adresse("Ledaweg zum Stier", "64","28359", "Bremen"));
+        verkaeufer.setAusweisNr("7654321");
 
         String datei = "H://LF02_//CSV/Kaufvertrag.csv";
         PrintWriter writer = new PrintWriter(datei);
         writer.print("Vertragspartner;");
         writer.print("Name;");
+        writer.print("Ausweisnummer;");
         writer.print("Strasse;");
-        writer.print("PLZ;");
+        writer.print("PLZ ");
         writer.println("Ort");
 
         writer.print("Verkaeufer;");
         writer.print(verkaeufer.getVorname()+" "+verkaeufer.getNachname()+";");
+        writer.print(verkaeufer.getAusweisNr()+";");
         writer.print(verkaeufer.getAdresse().getStrasse()+" "+verkaeufer.getAdresse().getHausNr()+";");
-        writer.print(verkaeufer.getAdresse().getPlz()+";");
+        writer.print(verkaeufer.getAdresse().getPlz()+" ");
         writer.println(verkaeufer.getAdresse().getOrt());
 
         writer.print("Kaeufer;");
         writer.print(kaeufer.getVorname()+" "+kaeufer.getNachname()+";");
+        writer.print(kaeufer.getAusweisNr()+";");
         writer.print(kaeufer.getAdresse().getStrasse()+" "+kaeufer.getAdresse().getHausNr()+";");
-        writer.print(kaeufer.getAdresse().getPlz()+";");
+        writer.print(kaeufer.getAdresse().getPlz()+" ");
         writer.println(kaeufer.getAdresse().getOrt());
 
         writer.close();
