4b14e5b3c7bc7846aed1db1df938fd31dadd62f7
christophk3 <christoph.k3@schule.bremen.de>
2022-05-18 11:07:00 +0200

    ok

--- sql_java/src/dao/VertragspartnerDAO.java
+++ sql_java/src/dao/VertragspartnerDAO.java
@@ -1,10 +1,76 @@
 package dao;
 
+import businessObjects.Adresse;
+import businessObjects.Vertragspartner;
+import org.sqlite.SQLiteException;
+
+import java.sql.*;
+
 public class VertragspartnerDAO {
     private final String CLASSNAME = "org.sqlite.JDBC";
     private final String CONNECTIONSRING = "jdbc:sqlite:sql_java/src/data/Kaufvertrag.db";
 
     public VertragspartnerDAO() throws ClassNotFoundException {
         Class.forName(CLASSNAME);
+    }
+
+    /**
+     * Liest einen Vertragspartner auf Basis seiner Ausweisnummer
+     *
+     * @param ausweisNr Die Ausweisnummer
+     * @return Der gewünschte Vertragspartner
+     */
+
+    public Vertragspartner read(String ausweisNr) {
+        Vertragspartner vertragspartner = null;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        // Verbindung zu Datenbank herstellen
+        try {
+            connection = DriverManager.getConnection(CONNECTIONSRING);
+
+
+            // SQL-Abfrage erstellen
+            String sql = "SELECT * FROM vertragspartner where ausweisNr = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, ausweisNr);
+
+            //SQL-Abfrage ausführen
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            // Zeiger auf den ersten Datensatz setzen
+            resultSet.next();
+
+            //ResultSet auswerten
+            String nr = resultSet.getString("ausweisNr");
+            String vorname = resultSet.getString("vorname");
+            String nachname = resultSet.getString("nachname");
+            String strasse = resultSet.getString("strasse");
+            String hausNr = resultSet.getString("hausNr");
+            String plz = resultSet.getString("plz");
+            String ort = resultSet.getString("ort");
+
+            // Vertragspartner erstellen
+            vertragspartner = new Vertragspartner(vorname, nachname);
+            vertragspartner.setAusweisNr(nr);
+            Adresse adresse = new Adresse(strasse, hausNr, plz, ort);
+            vertragspartner.setAdresse(adresse);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+
+            }
+        }
+
+        return vertragspartner;
+
+
     }
 }
--- sql_java/src/test.java
+++ sql_java/src/test.java
@@ -1,11 +1,15 @@
+import businessObjects.Vertragspartner;
 import dao.VertragspartnerDAO;
 import dao.WareDAO;
 
+import java.sql.SQLException;
+
 public class test {
-    public static void main(String[] args) throws ClassNotFoundException{
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         VertragspartnerDAO vertragspartnerDAO = new VertragspartnerDAO();
-        WareDAO wareDAO = new WareDAO();
-        System.out.println("super");
+
+        Vertragspartner vertragspartner = vertragspartnerDAO.read("0123456789");
+        System.out.println(vertragspartner.getVorname());
 
     }
 }
